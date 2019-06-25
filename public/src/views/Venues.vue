1054ef0a45724329ef52d2b814cbb70243a28ccc
路焱 <1090165836@qq.com>
2019-06-25 13:30:34 +0800

    add new

--- public/src/views/Venues.vue
+++ public/src/views/Venues.vue
@@ -115,11 +115,11 @@
           <div class="itemDetail">
             <router-link :to="`/venue_details/${t.vid}`" class="venueName">{{t.vname}}</router-link>
             <p class="venuePlace">{{t.vaddress}}</p>
-            <router-link :to="`/venue_details/${t.vid}`" class="venueCount">最近有<span>{{t.shows.length}}</span>场演出</router-link >
+            <router-link :to="`/venue_details/${t.vid}`" class="venueCount">最近有<span>{{venue_live.length}}</span>场演出</router-link >
             <div class="aboutList">
-              <router-link v-for="(t,i) of t.shows" :key="i" 
-              :to="`/live_details/${t.sid}`">
-                <img :src="t.sphoto" alt="">
+              <router-link v-for="(t,i) of arr" :key="i" 
+              :to="`/live_details/${venue_live[i].tid}`">
+                <img :src="venue_live[i].sphoto" alt="">
               </router-link>
             </div>
           </div>
@@ -146,29 +146,52 @@ export default {
   data(){
     return {
       venue_list:{},
-      venue_live:{},
-      vid:""
+      venue_live:[],
+      vid:"",
+      arr:[0,1,2]
     }
   },
-  created(){
+ created(){
+    // 获取现场列表
     this.axios.get(
       "venues/list"
     ).then(result=>{
       this.venue_list=result.data.result;
-      console.log(result.data);
-    })
-    this.axios.get(
+      for(let item of result.data.result){
+        //console.log(item);
+      // 获取现场下的演出列表
+      this.axios.get(
       "tours/list",
       {
         params:{
-          vid:this.vid,
-          }
+          vid:item.vid,
+        }
       }
     ).then(result=>{
-      this.venue_live=result.data;
-      console.log(result.data);
+      // console.log(result.data.result);
+      // this.venue_live.push(result.data.result);
+      // result.data.result
+      this.venue_live=result.data.result;
+      //console.log(this.venue_live);
+      //console.log(this.venue_live[0]);
     })
-  }
+        
+
+    }
+
+    
+    // console.log(Array.isArray(this.venue_live));
+    // this.venue_live = this.venue_live.slice(0,3)
+      // console.log(111);
+      // console.log(result.data);
+    })//.then(()=>{
+    //     console.log(this.venue_live[0]);
+    //     }
+    // )
+        // this.venue_live=this.venue_live.slice(0,3);
+           
+          
+  },
 }
 </script>
 <style scoped src="../../public/css/venue.css">
