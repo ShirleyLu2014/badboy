dbe1d674ff6bd22f20cda01a09716ada7af93c3c
zhangdong <zhangdong@tedu.cn>
2019-06-29 09:53:18 +0800

    add new

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors=require("cors");
-const session=require("express-session");
+//const session=require("express-session");
 /*引入路由模块*/
 const tours=require("./routes/tours");
 const artists=require("./routes/artists");
 const venues=require("./routes/venues");
 const cities=require("./routes/cities");
 const styles=require("./routes/styles");
+const user=require("./routes/user");
+/*引入token的模块*/
+const jwt=require("./jwt.js")
+
 
 var app = express();
 //新浪云要求只能监听5050
@@ -23,16 +27,34 @@ app.use(express.static('public'));
 //使用body-parser中间件
 app.use(bodyParser.urlencoded({extended:false}));
 //配置session
-app.use(session({
+/*app.use(session({
   secret: 'keyboard cat',
   resave: true,
   saveUninitialized: false
-}));
+}));*/
+app.use((req, res, next)=>{ 
+  if (req.url != '/user/login' && req.url.startsWith("/user")) {
+    let token = req.headers.token;
+    let result = jwt.verifyToken(token);
+    // 如果考验通过就next，否则就返回登陆信息不正确
+    if (result == 'exp') {
+      res.send({status: 403, msg: '登录超时，请重新登录'});
+    } else if (result.hasOwnProperty("JsonWebTokenError")){
+      res.send({status: 400, msg: '证书出错'})
+    } else{
+      req.user=result;
+      next();
+    }
+  } else {
+    next();
+  }
+});
 /*使用路由器来管理路由*/
 app.use("/tours",tours);
 app.use("/artists",artists);
 app.use("/venues",venues);
 app.use("/cities",cities);
 app.use("/styles",styles);
+app.use("/user",user);
 
 
